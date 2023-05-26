ad17d85bb91d60158fdc283cacec06f94de65f43
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-05-26 09:28:45 -0300

    tipos primitivos e diferenca de var e let

---
+++ aula09/index.js
@@ -0,0 +1,7 @@
+//ECMAScript 2015 (ES6)
+// var pode ser redeclarado
+// nome = 'luiz' nao botar varival sem declarar se e var ou let
+var nome = 'Luiz';
+var nome = 'Otavio';
+
+console.log(nome);
---
+++ aula10/index.js
@@ -0,0 +1,23 @@
+//tipos de dados primitivos
+
+//strings
+const nome = 'luiz';
+const nome1 = "luiz";
+const nome3 = `Luiz`;
+
+//number
+const num1 = 10;
+const num2 = 10.52;
+
+//undefined -> nao aponta para lugar nenhum na memoria
+let nomeAluno;
+
+//nulo -> nao aponta para local nenhum na memoria
+let sobrenomeAluno = null;
+
+//Boolean
+const boolean = false; // or true
+
+//verificando o tipo
+console.log(typeof nome, typeof num1, typeof nomeAluno, typeof sobrenomeAluno, typeof boolean);
+
