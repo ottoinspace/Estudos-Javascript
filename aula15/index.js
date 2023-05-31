b0d978a360298e7d49095689495b45f459bfc7a5
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-05-31 17:55:21 -0300

    aula 15 usando objeto math

---
+++ aula15/index.js
@@ -0,0 +1,24 @@
+let num1 = 9.137873;
+//arrendondar para baixo
+let num2 = Math.floor(num1);
+//arrendondar para cima
+let num3 = Math.ceil(num1);
+//arrendoddar para o mais proximo
+let num4 = Math.round(num1);
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+//pegar o numero maior e com min o menor
+console.log(Math.max(1, 2, 45, 786, -092, 93, 1000, 11837));
+
+//pegar numero aleatorio
+const aleatorio = Math.random() * (10 - 5) + 5;
+console.log(aleatorio)
+
+//exponenciacao
+console.log(Math.pow(2, 10));
+
