edcef57412501eb4bedd69e7bc5d8ef35cdd91dc
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-09 19:17:50 -0300

    aula de operadores de comporacao

---
+++ JavaScript Logica/aula21/index.js
@@ -0,0 +1,15 @@
+/*
+operadore de comparacao 
+
+> maior que
+>= mair que ou igual a
+< menor que
+== igualdade (valor) ***********
+=== igualdade restrita (valor e tipo)
+!= diferente (valor) ***********
+!== diferente restrito (valor e tipo)
+*/
+const num1 = 10; //number
+const num2 = '10'; // string
+const comp = num1 == num2;
+console.log(comp);
