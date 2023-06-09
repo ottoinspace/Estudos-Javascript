3419c055371200e8deb02a5a5066694a1054cf01
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-09 19:28:13 -0300

    aula de operadores logico

---
+++ JavaScript Logica/aula22/index.js
@@ -0,0 +1,14 @@
+/*
+operadores logicos
+
+&& -> AND -> E
+|| -> OR -> OU
+!  -> NOT - NAO
+*/
+
+const expressaoAnd = true && true;//todas as operacoes precisam ser true para ser true
+const expressaoOR = true || false; //apenas um true precisa para retornar true
+console.log(expressaoOR);
+
+//usando !
+console.log(!true);
