f507cfc8d3f7bef88ca1a00b7ed4c9be6915a9f8
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-10 17:27:13 -0300

    Calculadora imc terminada

--- JavaScript Logica/aula26/assets/js/main.js
+++ JavaScript Logica/aula26/assets/js/main.js
@@ -19,10 +19,29 @@ form.addEventListener('submit', function (event) {
         return;
     }
 
+    const imc = getImc(peso, altura);
+    const NivelImc = getNivelImc(imc);
 
-    console.log('Cheguei aqui')
-    //continua o codigo
+    const msg = `Seu IMC e ${imc} (${NivelImc})`;
+
+    setResultado(msg, true);
 });
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesudade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel [5]; 
+    if (imc >= 34.9) return nivel [4];
+    if (imc >= 29.9) return nivel [3];
+    if (imc >= 25.9) return nivel [2]; 
+    if (imc >= 18.5) return nivel [1];
+    if (imc < 18.5) return nivel [0];
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP () {
     const p = document.createElement('p');
@@ -34,6 +53,12 @@ function setResultado (msg, isValid) {
     resultado.innerHTML = '';
   
     const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
   
     p.innerHTML = msg;
     resultado.appendChild(p);
