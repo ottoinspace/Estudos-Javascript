ccf9ca346bfff4b6c196062b7ce8430d3d213b29
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-13 15:30:32 -0300

    objeto Date

--- JavaScript Logica/aula27/index.js
+++ JavaScript Logica/aula27/index.js
@@ -1,4 +1,4 @@
-// ? :
+// (condicao) ? 'Valor para verdadeiro' : 'Valor para falso'
 const pontuacaoUsuario = 999;
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
 console.log(nivelUsuario);
