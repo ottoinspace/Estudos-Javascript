45d5d8d012061eda28061fadec15d61be1dd4907
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-13 14:20:21 -0300

    operacao ternaria

---
+++ JavaScript Logica/aula27/index.js
@@ -0,0 +1,11 @@
+// ? :
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+console.log(nivelUsuario);
+
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log ('Usuario VIP');
+// } else {
+//     console.log('Usuario normal');
+// }
