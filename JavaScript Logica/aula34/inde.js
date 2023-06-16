a31ad03cb4a3e3edc6fdab1857bed821d043ee34
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-16 16:05:16 -0300

    aula 34 usando for

---
+++ JavaScript Logica/aula34/inde.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i < 6; i++) {
+//     console.log(`linha ${i}`);
+// }
+
+const frutas = ['Maca', 'Pera', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}, ${frutas[i]}`);
+}
