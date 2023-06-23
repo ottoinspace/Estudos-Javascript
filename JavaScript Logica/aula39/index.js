0cc753992b887c489d9d5025e172db5d2ebeda6c
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-23 11:45:41 -0300

    aula39 while e do while

---
+++ JavaScript Logica/aula39/index.js
@@ -0,0 +1,20 @@
+function random(min, max) {
+    const r = Math.random() * (max- min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50
+let rand = random(min, max);
+
+while (rand !== 10){
+    rand = random(min, max);
+    console.log(rand);
+}
+
+
+//sempre execuar o codigo pelomenos uma vez
+do {
+    rand = random(min, max);
+    console.log(rand);
+}while(rand !== 10);
