837676706aaf2ef427f6497b37b5ce5d3831fff1
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-23 16:08:58 -0300

    aula44 try, catch e throw

---
+++ JavaScript Logica/aula44/index.js
@@ -0,0 +1,15 @@
+function soma (x, y){
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new ReferenceError('x e y precisam ser numeros.');
+    }
+
+    return x + y;
+}
+
+try{
+    console.log(soma(1, 2));
+    console.log(soma('1', 2));
+}catch(error) {
+   // console.log(error);
+   console.log('alguma coisa mais amigavel pro usuario.');
+}
