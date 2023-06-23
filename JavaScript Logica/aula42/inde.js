7949db525e911c7560f7574a21e4ddc68121bbb8
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-23 15:47:43 -0300

    aula42 exercicio para identificar se a imagem e paisagem ou nao

---
+++ JavaScript Logica/aula42/inde.js
@@ -0,0 +1,4 @@
+// Escreva uma funcao chamada ePaisagem que recebe dois argumentos, largura e altura de imagem (number). Retorne true se a imagem estiver no modo paisagem.
+const ePaisagem = (largura, altura) => largura >= altura;
+
+console.log(ePaisagem(1080,1920));
