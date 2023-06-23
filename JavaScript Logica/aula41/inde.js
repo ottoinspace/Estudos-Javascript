e7ea4a02e8e267c01a40edfa1c373cf511184bb6
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-06-23 15:25:03 -0300

    aula41 exercico para fazer uma funcao retornando o maior numero

---
+++ JavaScript Logica/aula41/inde.js
@@ -0,0 +1,18 @@
+// Escreva uma funcao que receba 2 numeros e retorne o maior deles
+
+// function maiorQue (x, y) {
+//     if (x > y) return x;
+//     return y;
+// }
+
+// function maiorQue (x, y) {
+//     return x > y ? x : y;
+// }
+
+// const maiorQue = (x, y) => {
+//     return x > y ? x : y; 
+// }
+
+const maiorQue = (x, y) => x > y ? x : y; 
+
+console.log(maiorQue(5,2));
