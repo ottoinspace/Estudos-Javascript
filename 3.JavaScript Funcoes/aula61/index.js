75b5a0e91512b4db81c8b66e76d327417d891ad7
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-03 21:48:55 -0300

    aula61 funcao geradora

---
+++ 3.JavaScript Funcoes/aula61/index.js
@@ -0,0 +1,54 @@
+function* geradora1() {
+    // codigo qualquer...
+    yield 'valor 1';
+    // codigo qualquer...
+    yield 'valor 2';
+    // codigo qualquer...
+    yield 'valor 3';
+}
+
+function* geradora2() {
+    let i = 0;
+
+    while(true) {
+        yield i;
+        i++
+    }
+}
+
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+
+function* geradora5() {
+    yield function() {
+        console.log('Vim do y1');
+    };
+
+    // return function() {
+    //     console.log('VIm do return');
+    // }
+
+    //...
+    yield function() {
+        console.log('Vim do y2');
+    };
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+
+func1();
+func2();
