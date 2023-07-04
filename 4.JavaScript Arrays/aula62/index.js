19d2f74f4050681a3468e64eb2d1bc63d4964611
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-03 22:02:04 -0300

    aula62 revisao de arrays

---
+++ 4.JavaScript Arrays/aula62/index.js
@@ -0,0 +1,28 @@
+//valor por referencia
+// const nomes = new Array('Eduardo', 'Maria', 'Joana');
+// const novo = [...nomes];
+
+// novo.pop();
+// console.log(nomes);
+// console.log(novo)
+
+// console.log(nomes.length);
+
+// const removido = nomes.shift();
+// console.log(nomes, removido);
+
+// nomes.unshift('Joao'); // adiciona no comeco
+// nomes.push('Wallace');// adiciona no final
+// console.log(nomes);
+
+// const novo = nomes.slice(0, 2);
+// console.log(novo);
+
+// const nome = 'Luiz Otavio Miranda';
+// const nomes = nome.split(' ');
+// console.log(nomes);
+
+const nomes = ['Luiz', 'Otavio', 'Miranda'];
+const nome = nomes.join(' ');
+
+console.log(nome);
