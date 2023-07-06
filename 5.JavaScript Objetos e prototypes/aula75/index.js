6537d31750723d971bbc9b22774e0dea11cd19af
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-06 18:02:18 -0300

    aula75 manipulando prototypes

---
+++ 5.JavaScript Objetos e prototypes/aula75/index.js
@@ -0,0 +1,36 @@
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+// literal
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.aumento(10);
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 99
+    }
+});
+p3.aumento(34);
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
