957e47c042408f330545040ef808532f75a60003
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-13 20:22:15 -0300

    aula94 import/export

---
+++ 8.JavaScript tooling e ES6 Modules/aula94/src/index.js
@@ -0,0 +1,4 @@
+// import { nome as nome2, sobrenome, idade, soma } from './modulo1';
+import * as meuModulo from './modulo1';
+
+console.log(meuModulo);
---
+++ 8.JavaScript tooling e ES6 Modules/aula94/src/modulo1.js
@@ -0,0 +1,8 @@
+export const nome = 'Luiz';
+export const sobrenome = 'Miranda';
+export const idade = 30;
+
+export function soma(x, y) {
+    return x + y;
+}
+
